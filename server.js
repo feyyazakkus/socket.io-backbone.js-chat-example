7ef5754ccf772e5df0827aceb88784c3d94ae000
giray sam <gry.sam@gmail.com>
2016-11-02 14:27:51 +0200

    fix: information message color

--- server.js
+++ server.js
@@ -37,7 +37,6 @@ io.on('connection', function (socket) {
         date = new Date(data.date);
         data.date = pad(date.getHours()) + ':' + pad(date.getMinutes());
 
-        console.log(data);
         io.emit('message', data);
     });
 
